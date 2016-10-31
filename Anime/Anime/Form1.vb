3c16e8c563b6ff4b4a8d4b48f80e55fce2b5ec7d
Lyon540 <lmgc183@gmail.com>
2016-10-31 12:24:42 -0600

    o.e

---
+++ Anime/Anime/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Btn_Cerrar_Click(sender As Object, e As EventArgs) Handles Btn_Cerrar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Btn_Añadir_Click(sender As Object, e As EventArgs) Handles Btn_Añadir.Click
+        Dim conexion As SqlConnection
+        conexion = New SqlConnection("server=. ; database=Anime ; integrated security = true")
+        conexion.Open()
+        Dim descri As String = TextBox1.Text
+        Dim precio As String = TextBox2.Text
+        Dim cadena As String = "insert into articulos(descripcion,precio) values ('" & descri & "'," & precio & ")"
+        Dim comando As SqlCommand
+        comando = New SqlCommand(cadena, conexion)
+        comando.ExecuteNonQuery()
+        MessageBox.Show("Los datos se guardaron correctamente")
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        conexion.Close()
+    End Sub
+End Class
